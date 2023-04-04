fc4f3d6ad8ace1abe84545de055a97bd62a15197
Rashmi Gupta <rashmimittal2497@gmail.com>
2023-04-04 18:15:41 +0530

    learnt database seeding, adding dbcontext service

---
+++ MVCMovie/Models/SeedData.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+using MVCMovie.Data;
+
+namespace MVCMovie.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var cntxt = new MVCMovieContext(serviceProvider.GetRequiredService<DbContextOptions<MVCMovieContext>>()))
+            {
+                //Look out for any movies
+                if (cntxt.Movie.Any())
+                    return;    //Db has been seeded
+
+                cntxt.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "ZNMD",
+                        ReleaseDate = DateTime.Parse("2012-02-02"),
+                        Genre = "Romantic Comedy",
+                        Price = 200
+                    },
+                    new Movie
+                    {
+                        Title = "RHTDM",
+                        ReleaseDate = DateTime.Parse("2002-12-22"),
+                        Genre = "Romantic Comedy",
+                        Price = 300
+                    },
+                    new Movie
+                    {
+                        Title = "Bhagat Singh",
+                        ReleaseDate = DateTime.Parse("2003-11-23"),
+                        Genre = "Patriotic",
+                        Price = 250
+                    },
+                    new Movie
+                    {
+                        Title = "AntMan",
+                        ReleaseDate = DateTime.Parse("2015-11-23"),
+                        Genre = "Thriller",
+                        Price = 350
+                    });
+                cntxt.SaveChanges();
+            }
+        }
+    }
+}
--- MVCMovie/Program.cs
+++ MVCMovie/Program.cs
@@ -1,21 +1,37 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+
+using MVCMovie.Data;
+using MVCMovie.Models;
+
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 
 namespace MVCMovie
 {
     public class Program
     {
-       static IHostBuilder builder;
+        //static IHost host;
+
         public static void Main(string[] args)
         {
-            builder = CreateHostBuilder(args);
-            builder.Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    SeedData.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+        host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -24,8 +40,5 @@ namespace MVCMovie
                 {
                     webBuilder.UseStartup<Startup>();
                 });
-
-    //    builder.Services.AddDbContext<MvcMovieContext>(options =>
-    //options.UseSqlServer(builder.Configuration.GetConnectionString("MvcMovieContext")));
     }
 }
